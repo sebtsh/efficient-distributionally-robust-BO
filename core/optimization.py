c64edf4278b6d93725f4e3c2f684ced7659df7a2
seb <seb.tsh@gmail.com>
2021-07-21 01:24:42 +0800

    Add distributionally robust Bayes opt loop

--- core/optimization.py
+++ core/optimization.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+from typing import Callable
 from tqdm import trange
 from trieste.data import Dataset
 from trieste.observer import SingleObserver
@@ -51,6 +54,72 @@ def bayes_opt_loop(model: ModelOptModule,
 
         # Get model's belief of maximizer at this timestep
         maximizers.append(get_maximizer(model.gp, search_points))
+
+        # Save model parameters at this timestep
+        model_params.append(model.get_params())
+
+    return dataset, maximizers, model_params
+
+
+def bayes_opt_loop_dist_robust(model: ModelOptModule,
+                               init_dataset: Dataset or None,
+                               search_points: TensorType,
+                               context_points: TensorType,
+                               observer: SingleObserver,
+                               acq: Acquisition,
+                               num_iters: int,
+                               reference_dist_func: Callable,
+                               true_dist_func: Callable,
+                               margin_func: Callable,
+                               optimize_gp: bool = True):
+    """
+    Main distributionally robust Bayesian optimization loop.
+    :param model:
+    :param init_dataset:
+    :param search_points:
+    :param context_points:
+    :param observer:
+    :param acq:
+    :param num_iters:
+    :param reference_dist_generator: A function that takes in a timestep and returns an array of shape |C|, the size
+    of the context set that is a valid probability distribution (sums to 1).
+    :param true_dist_generator: A function that takes in a timestep and returns an array of shape |C|, the size
+    of the context set that is a valid probability distribution (sums to 1).
+    :margin_func: A function that takes in a timestep and returns epsilon_t
+    :param optimize_gp:
+    :return:
+    """
+    dataset = init_dataset
+    maximizers = []
+    model_params = []
+    for t in trange(num_iters):
+        # Get reference distribution
+        ref_dist = reference_dist_func(t)
+        epsilon = margin_func(t)
+
+        # Acquire next input locations to sample
+        action = acq.acquire(model, search_points, t, ref_dist, epsilon)  # TensorType of shape (1, d_x)
+        if action is None:  # Early termination signal
+            print("Early termination at timestep t={}".format(t))
+            return dataset, maximizers, model_params
+
+        # Environment samples context from true distribution
+        true_dist = true_dist_func(t)
+        context = context_points[np.random.choice(len(context_points), p=true_dist)][None, :]  # (1, d_c)
+
+        # Obtain observations
+        next_input = np.concatenate([action, context], axis=-1)
+        next_dataset = observer(next_input)
+        if dataset is not None:
+            dataset = dataset + next_dataset
+        else:
+            dataset = next_dataset
+
+        # Update model's dataset and optimize
+        model.update_dataset(dataset)
+
+        if optimize_gp:
+            model.optimize()
 
         # Save model parameters at this timestep
         model_params.append(model.get_params())
