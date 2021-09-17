5fa2c37e6ed72814304b5f8b1a4604ad1c877e3b
seb <seb.tsh@gmail.com>
2021-09-17 16:44:38 +0800

    Add cumulative reward plotting

--- metrics/wind_results.py
+++ metrics/wind_results.py
@@ -26,7 +26,7 @@ def main(obj_func_name, num_init_points, num_months, num_bo_iters, beta, seed, s
     indiv_results_dir = "runs/" + obj_func_name + "/indiv_results/"
     Path(dir).mkdir(parents=True, exist_ok=True)
 
-    divergences = ['MMD', 'MMD_approx', 'TV', 'modified_chi_squared']
+    divergences = ['MMD_approx', 'TV', 'modified_chi_squared']
     acquisitions = ['GP-UCB', 'DRBOGeneral', 'DRBOWorstCaseSens', 'DRBOMidApprox']
 
     color_dict = {'GP-UCB': '#d7263d',
@@ -36,18 +36,22 @@ def main(obj_func_name, num_init_points, num_months, num_bo_iters, beta, seed, s
 
     for divergence in divergences:
         x = np.arange(num_bo_iters)
-
-        fig, axs = plt.subplots(1, 2, figsize=figsize, dpi=dpi)
+        # Regret plot
+        regret_fig, regret_axs = plt.subplots(1, 2, figsize=figsize, dpi=dpi)
         plot_name = "{}-{}-beta{}".format(obj_func_name,
                                           divergence,
                                           beta)
         print("Plotting " + plot_name)
-        axs[0].set_title(plot_name + ": Immediate")
-        axs[1].set_title(plot_name + ": Cumulative")
+        regret_axs[0].set_title(plot_name + ": Immediate")
+        regret_axs[1].set_title(plot_name + ": Cumulative")
+        # Reward plot
+        reward_fig, reward_axs = plt.subplots(figsize=figsize, dpi=dpi)
+
         for acquisition in acquisitions:
             color = color_dict[acquisition]
             all_regrets = np.zeros((num_months, num_bo_iters))
             all_cumu_regrets = np.zeros((num_months, num_bo_iters))
+            all_cumu_rewards = np.zeros((num_months, num_bo_iters))
             all_times = []
             for month in range(num_months):
                 file_name = "{}-month{}-{}-{}-seed{}-beta{}.p".format(obj_func_name,
@@ -56,42 +60,56 @@ def main(obj_func_name, num_init_points, num_months, num_bo_iters, beta, seed, s
                                                                       acquisition,
                                                                       seed,
                                                                       beta)
-                regrets, cumulative_regrets, average_acq_time, query_points = pickle.load(
+                regrets, cumulative_regrets, average_acq_time, query_points, rewards, cumulative_rewards = pickle.load(
                     open(indiv_results_dir + file_name, "rb"))
                 # cut out initial points
                 regrets = np.array(regrets[num_init_points:])
                 base_cumulative_regret = cumulative_regrets[num_init_points - 1]
                 cumulative_regrets = np.array(cumulative_regrets[num_init_points:]) - base_cumulative_regret
+                cumulative_rewards = np.array(cumulative_rewards)[num_init_points:]
 
                 # cut out anything above minimum number of months for uniformity across months
                 regrets = regrets[:num_bo_iters]
                 cumulative_regrets = cumulative_regrets[:num_bo_iters]
+                cumulative_rewards = cumulative_rewards[:num_bo_iters]
 
                 all_regrets[month] = regrets
                 all_cumu_regrets[month] = cumulative_regrets
+                all_cumu_rewards[month] = cumulative_rewards
                 all_times.append(average_acq_time)
+
             mean_regrets = np.mean(all_regrets, axis=0)
             std_err_regrets = np.std(all_regrets, axis=0) / np.sqrt(num_months)
             mean_cumu_regrets = np.mean(all_cumu_regrets, axis=0)
             std_err_cumu_regrets = np.std(all_cumu_regrets, axis=0) / np.sqrt(num_months)
+            mean_cumu_rewards = np.mean(all_cumu_rewards, axis=0)
+            std_err_cumu_rewards = np.std(all_cumu_rewards, axis=0) / np.sqrt(num_months)
 
             # Immediate regret
-            axs[0].plot(x, mean_regrets, label=acquisition, color=color)
-            axs[0].fill_between(x, mean_regrets - std_err_regrets, mean_regrets + std_err_regrets,
+            regret_axs[0].plot(x, mean_regrets, label=acquisition, color=color)
+            regret_axs[0].fill_between(x, mean_regrets - std_err_regrets, mean_regrets + std_err_regrets,
                                 alpha=0.2, color=color)
-            axs[0].legend()
+            regret_axs[0].legend()
 
             # Cumulative regret
-            axs[1].plot(x, mean_cumu_regrets, label=acquisition, color=color)
-            axs[1].fill_between(x, mean_cumu_regrets - std_err_cumu_regrets,
+            regret_axs[1].plot(x, mean_cumu_regrets, label=acquisition, color=color)
+            regret_axs[1].fill_between(x, mean_cumu_regrets - std_err_cumu_regrets,
                                 mean_cumu_regrets + std_err_cumu_regrets,
                                 alpha=0.2, color=color)
-            axs[1].legend()
+            regret_axs[1].legend()
+
+            # Cumulative rewards
+            reward_axs.plot(x, mean_cumu_rewards, label=acquisition, color=color)
+            reward_axs.fill_between(x, mean_cumu_rewards - std_err_cumu_rewards,
+                                mean_cumu_rewards + std_err_cumu_rewards,
+                                alpha=0.2, color=color)
+            reward_axs.legend()
 
             # Average acquisition time
             print("{}-{} average acquisition time in seconds: {}".format(divergence,
                                                                          acquisition,
                                                                          np.mean(all_times)))
-        fig.savefig(dir + plot_name + "-regret.png")
+        regret_fig.savefig(dir + plot_name + "-regret.png")
+        reward_fig.savefig(dir + plot_name + "-reward.png")
     if show_plots:
         plt.show()
