0565a5310b66d7204fc143d096b95fbbad2b841d
seb <seb.tsh@gmail.com>
2021-10-06 16:42:35 +0800

    Change portfolio_bigexp.py

--- experiments/portfolio_bigexp.py
+++ experiments/portfolio_bigexp.py
@@ -38,12 +38,12 @@ def portfolio():
     context_uppers = [1] * context_dims
     action_density_per_dim = 10
     context_density_per_dim = 10
-    ls = [0.1, 0.1, 0.1, 0.1]
+    ls = [0.1, 0.5, 0.1, 0.5]
     obs_variance = 0.001
     is_optimizing_gp = False
     opt_max_iter = 10
     num_bo_iters = 500
-    num_init_points = 20
+    num_init_points = 10
     beta_const = 2
     ref_var = 0.05
     seed = 0
